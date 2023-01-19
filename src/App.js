ebd02d96bb15c3042b213d6cbfb64908ae2a3cca
unknown <rahularora993676@gmail.com>
2023-01-19 06:10:51 -0800

    redux

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { useSelector,useDispatch } from 'react-redux';
+import { incNumber,decNumber } from './actions';
+import { ChangeDark } from './actions';
+
+// import { decNumber } from './actions';
+
+
+function App() {
+  const myState=useSelector((state)=>state.changeTheNumber)
+  const changeThemeApp=useSelector((state)=>state.darkReducer)
+  const dispatch=useDispatch()
+  return (
+ 
+    <div className="App" style={{
+      backgroundColor: `${changeThemeApp ? "black" : "white"}`,
+      color: `${changeThemeApp ? "white" : "black"}`,
+    }}>
+     <h1>Increment/Decrement </h1>
+     <button className='btn1' onClick={()=>dispatch(incNumber())}>Increment</button>
+     <h1>{myState}</h1>
+     <button className='btn2' onClick={()=>dispatch(decNumber())} >Decrement</button>
+     <button className='btn2' onClick={()=>dispatch(ChangeDark())}>themeChange</button>
+    </div>
+  );
+}
+
+export default App;
